b4d9c3e0e7eef428a5800e1345cc7fee55d0da41
Kazuya Nagatsukasa <nagatsukasa7@gmail.com>
2025-11-07 12:37:29 +0900

    登録画面に戻るver

---
+++ appori.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/janken2", (req, res) => {
+  const hand = req.query.radio ;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
+  let cpu = '';
+  let judgement = '';
+  if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
+  else cpu = 'パー';
+  // ここに勝敗の判定を入れる
+  // 以下の数行は人間の勝ちの場合の処理なので，
+  // 判定に沿ってあいこと負けの処理を追加する
+  judgement = '勝ち';
+  win += 1;
+  total += 1;
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'janken', display );
+});
+
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
